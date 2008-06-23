ea776fb83d99f2f4b8d90ba86bafc4dd36b0aee9
shashank.personal@gmail.com <shashank.personal@gmail.com@210d2194-134c-0410-861e-6ff51eae6dea>
2008-06-23 05:41:27 +0000

    missed this file by mistake

---
+++ trunk/datastructure.h
@@ -0,0 +1,26 @@
+//
+// C++ Interface: datastructure
+//
+// Description: 
+//
+//
+// Author: Shashank Singh <shashank.personal@gmail.com>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+class dataStructure
+{
+
+};//the base class of any json data Structure
+
+class twitterDataStructure : public dataStructure 
+{
+
+};//twitter data structure
+
+class panoramioDataStructure : public dataStructure 
+{
+
+};//this class is here just because i need it somewhere else :)
