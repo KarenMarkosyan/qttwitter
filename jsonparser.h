ee9438665205e2ea2eede5c58f53b0d44908667a
shashank.personal <shashank.personal@gmail.com>
2008-06-23 05:37:16 +0000

    beautification of jsonparser with some features added ..this should be considered interim change before i am make bigger changes

--- jsonparser.h
+++ jsonparser.h
@@ -2,26 +2,23 @@
 #define JSONPARSER_H
 #include <QtScript/QScriptValue>
 #include <QtScript/QScriptEngine>
-//
-class dataStructure{};//the base class of any json data Structure
-class twitterDataStructure : public dataStructure {};
-class panoramioDataStructure : public dataStructure {};//this class is here just because i need it somewhere else :)
-//
+#include <datastructure.h>
+
 class jsonParser
 {
 public:
         jsonParser();
 
-        ~jsonParser();
+//         ~jsonParser();
 
-virtual dataStructure * parseObjectOnPosition ( const QString &content ,int requiredObjectPosition ) = 0;//for parsing single object
-//
+virtual dataStructure  parseObjectOnPosition ( const QString &content ,int requiredObjectPosition ) = 0;//for parsing single object
+
 virtual QList <dataStructure > parseAllObjects ( const QString &content,int numberOfObjects ) = 0;//for parsing a list objects
-//
-private:
-        QList <panoramioDataStructure> parsedJsonOutput;
 
-        panoramioDataStructure dataStorage;
+private:
+        QList <dataStructure> parsedJsonOutput;
+
+        dataStructure dataStorage;
 
         QScriptEngine myEngine;
 
