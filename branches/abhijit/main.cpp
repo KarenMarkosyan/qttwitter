8ff7187075beb412381ae2305af5de71cba04297
shashank.personal@gmail.com <shashank.personal@gmail.com@210d2194-134c-0410-861e-6ff51eae6dea>
2008-12-11 05:58:40 +0000

    Creating a private branch for abhijeet to experiment stuff as muhc as he want ,,welcome murli to qttwitter

---
+++ branches/abhijit/main.cpp
@@ -0,0 +1,46 @@
+/* this program is developed on QDevelop -- a good Qt IDE by Jean - Luc Biord and others :) & Kdevelop also
+qt used = Qt Open Source Edition version 4.4.1-snapshot-20080419.
+on open suse 10.3
+[02:53:53 PM Tuesday, December 09 2008] update i am using qt 4.4.3 and qt creator/kate/vim and ubuntu
+*/
+#include <QApplication>
+#include "MainWindow/mainwindowimpl.h"
+#include "loginDerived.h"
+#include "ui_sendTwit.h"
+#include "twitterbackendinterface.h"
+// #include "imagebrowserimpl.h"
+#include <QDialog>
+
+
+int main(int argc,char **argv)
+{
+	//declarations
+	QApplication app(argc,argv);
+	//this is derived from ui file and the added functionality  system tray MainWindow Class
+	MainWindowImpl mainWindow;
+	QDialog containerLoginDialog,containerSendTwitDialog,containerImageBrowser;
+        Ui::loginDialogDerived myLoginDialog;
+	Ui::sendTwitDialog mySendTwitDialog;
+// 	Ui::imageBrowserimpl myImageBroswer; //not needed right now
+        Backend::twitterBackend myTwitter;
+	
+	//connection slots
+	QObject::connect(&mainWindow,SIGNAL(quit()),&app,SLOT(quit()));//just quit when you get quit signal
+	QObject::connect(mainWindow.actionLogin,SIGNAL(triggered()),&containerLoginDialog,SLOT(show()));
+	QObject::connect(mainWindow.actionSet_New_Status,SIGNAL(triggered()),&containerSendTwitDialog,SLOT(show()));	
+	//QObject::connect(&containerSendTwitDialog,SLOT(
+	QObject::connect(mainWindow.actionPublic,SIGNAL(triggered()),&myTwitter,SLOT(public_timeline()));
+	QObject::connect(mainWindow.actionFriends,SIGNAL(triggered()),&myTwitter,SLOT(friends_timeline()));
+        QObject::connect(mainWindow.actionOwn,SIGNAL(triggered()),&myTwitter,SLOT(user_timeline()));
+        QObject::connect(mainWindow.actionView_Images_From_Area,SIGNAL(triggered()),&containerImageBrowser,SLOT(show()));
+        
+//         QObject::connect(&myLoginDialog, , );
+        QObject::connect(&myTwitter,SIGNAL(public_timeline(QString)),mainWindow.textLabelMainWindow,SLOT(setText(QString)) );//connection for shwoing public timeline
+	//QObject::connect(mainWindow.action,SIGNAL(triggered()),&myTwitter,SLOT());
+	//property setting
+	myLoginDialog.setupUi(&containerLoginDialog);
+        mySendTwitDialog.setupUi(&containerSendTwitDialog);
+//     myImageBroswer.setupUi(&containerImageBrowser);
+	mainWindow.show();
+	return app.exec();
+}
---
+++ branches/abhijit/twitterbackendinterface.cpp
@@ -0,0 +1,198 @@
+#include "twitterbackendinterface.h"
+//
+twitterBackendInterface::twitterBackendInterface( )
+        : QObject()
+{
+    m_twitLib = new QTwitLib ;
+    isLogin = false;
+    connect(m_twitLib,SIGNAL(OnResponseReceived(Returnables::Response *)),this,SLOT(OnResponseReceived(Returnables::Response *)));
+}
+
+//
+void twitterBackendInterface::public_timeline ( /*int since_id*/ )
+{
+    m_twitLib->GetPublicTimeline();
+    qDebug() << "public_timeline";
+}
+
+void  twitterBackendInterface::friends_timeline()
+{
+    qDebug() << " friends";
+}
+
+void  twitterBackendInterface::user_timeline()
+{
+    qDebug() << "user ";
+}
+
+void  twitterBackendInterface::showStatus()
+{
+
+}
+
+void twitterBackendInterface::update()
+{
+}
+
+void  twitterBackendInterface::replies()
+{
+}
+
+void  twitterBackendInterface::destroy()
+{
+}
+
+void  twitterBackendInterface::friends()
+{
+}
+
+void  twitterBackendInterface::followers()
+{
+}
+
+void  twitterBackendInterface::featured()
+{
+}
+
+void  twitterBackendInterface::showUser()
+{
+}
+
+void  twitterBackendInterface::direct_messages()
+{
+}
+
+void  twitterBackendInterface::sent()
+{
+}
+
+void  twitterBackendInterface::newDirectMessage()
+{
+}
+
+void  twitterBackendInterface::destroyDirectMessage()
+{
+}
+
+void  twitterBackendInterface::createFriendship()
+{
+}
+
+void  twitterBackendInterface::destroyFriendship()
+{
+}
+
+void  twitterBackendInterface::verify_credentials()
+{
+}
+
+void  twitterBackendInterface::end_session()
+{
+}
+
+void  twitterBackendInterface::archive()
+{
+}
+
+void  twitterBackendInterface::update_location()
+{
+}
+
+void  twitterBackendInterface::update_delivery_device()
+{
+}
+
+void  twitterBackendInterface::createFavourite()
+{
+}
+
+void  twitterBackendInterface::destroyFavourite()
+{
+}
+
+void  twitterBackendInterface::follow()
+{
+}
+
+void  twitterBackendInterface::leave()
+{
+}
+
+void  twitterBackendInterface::createBlockage()
+{
+}
+
+void  twitterBackendInterface::destroyBlockage()
+{
+}
+
+void  twitterBackendInterface::test()
+{
+}
+
+void  twitterBackendInterface::downtime_schedule()
+{
+}
+
+void twitterBackendInterface::OnError(QString error)
+{
+}
+void twitterBackendInterface::OnMessageReceived(QString message)
+{
+}
+void twitterBackendInterface::OnStatusReceived(SERVER::RESP response)
+{
+  
+}
+void twitterBackendInterface::OnResponseReceived(Returnables::Response *resp)
+{
+//   qDebug()<<"status recived";
+  
+  if(resp)
+  {
+    switch(resp->reqID)
+    {
+      case Returnables::PUBLIC_TIMELINE:
+      {
+        Returnables::PublicTimeline *pTimeline = static_cast<Returnables::PublicTimeline *>(resp);
+        DisplayList(pTimeline->list, "Public Timeline");
+        delete pTimeline;
+        break;
+      }
+    }
+  }
+      
+  
+}
+
+void twitterBackendInterface::DisplayList(QLinkedList<Returnables::StatusUser*> list, QString header)
+{
+  Returnables::StatusUser *statusUser = NULL;
+  QString value;
+
+  while(!list.isEmpty())
+  {
+    statusUser = list.takeFirst();
+//     value="ID:"+QString::number(statusUser->status.id) ;
+    value=statusUser->user.screenName+" twittered \" ";
+    value+=statusUser->status.text +" \" <br>";
+
+  }
+          emit(public_timeline(value));
+
+}
+void twitterBackendInterface::setUserNamePassword(QString user , QString password)
+{
+}
+
+// void twitterBackendInterface::OnLoginStatus ( bool isLoggedIn )
+// {
+//     if ( isLoggedIn )
+//     {
+//         isLogin = TRUE;
+//     }
+//     else
+//     {
+//         isLogin = FALSE;
+//     }
+// }
