7fa59b4f5160fb76a0e4230799ef1e3ef2c3e9b0
shashank.personal <shashank.personal@gmail.com>
2008-12-19 09:15:03 +0000

    derived class of twit dialog finished

--- sendTwitDerived.h
+++ sendTwitDerived.h
@@ -1,5 +1,23 @@
 #ifndef SENDTWIT_DERIVED
 #define SENDTWIT_DERIVED
+#include "ui_sendTwit.h"
 
+class SendTwitDerived : public QDialog , public Ui::sendTwitDialog
+{
+  Q_OBJECT
+
+  public:
+    SendTwitDerived();
+  public slots:
+    void recieveTwit(QString value);
+  signals:
+    void sendTwit(QString value);
+    
+};
+
+
+namespace Ui {
+  class sendTwitDerived: public SendTwitDerived {};
+}
 
 #endif
