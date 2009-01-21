d7bf23a205e5f97558093ab6ff47dda17f52e0a0
anand.chirag <anand.chirag@210d2194-134c-0410-861e-6ff51eae6dea>
2009-01-21 19:48:17 +0000

    Creating branch for chirag to fuck

---
+++ branches/chirag/sendTwitDerived.cpp
@@ -0,0 +1,15 @@
+#include "sendTwitDerived.h"
+
+
+SendTwitDerived::SendTwitDerived()
+{
+  setupUi(this);
+  QObject::connect(this, SIGNAL(accepted()), this, SLOT(recieveTwit()));
+}
+
+
+void SendTwitDerived::recieveTwit()
+{
+//   qDebug()<<label->text();
+  emit sendTwit(twitLineEdit->text());
+}
