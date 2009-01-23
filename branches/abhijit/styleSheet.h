ddd9b61c009bc4206c998fa3cb12aa20ffec1e82
abhijeet.anand.cool <abhijeet.anand.cool@210d2194-134c-0410-861e-6ff51eae6dea>
2009-01-23 21:07:43 +0000

    Have made extensive use of styleSheets. Added a Brushed Metal look to the toolBars, statusBars and the Menus, and the textBrowsers. But still not able to put background image to the main menuBar. Only the empty spaces at the end of the menuBar display the bg_image, and not where the menu text is present. Need to look into it to FIX it. Fixed the problem where upon resizing the main window, the text Browsers still remained in their fixedMinimum shape and did not expand with the main window resize. Now its working great. Also fixed along with it the proper display of ScrollBars. But on putting a background image to the QMainWindow, the scrollbars start behaving abnormally. But anyway, now that background for mainWindow is not required. Have updated few PNGs to make their backgrounds transparent. Have CHANGED the SYSTEM_TRAY_ICON to brand new icon, which I created myself alongwith the brushed metal backgroung image :-).

--- branches/abhijit/styleSheet.h
+++ branches/abhijit/styleSheet.h
@@ -1,91 +1,96 @@
-QString globalStyle =" QPushButton { \
-                     border: 1px solid #313131; \
-                     border-radius: 4px;\
-                     background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\
-                     stop: 0 #a5a5a5, stop: 0.8 #4d4d4d, stop: 1 #545454);\
-                     padding: 4px 12px;\
-                     color: #eeeeee;\
-                     font-weight: bold;\
-                     }\
-                     QPushButton:disabled,\
-                     QPushButton:default:disabled {\
-                     color: rgba(255,255,255,80);\
-                     border: 1px solid rgba(0,0,0,80);\
-                     background: none;\
-                     }\
-                     QPushButton:default:pressed,   QPushButton:pressed {\
-                     border: 1px solid #0e5a00;\
-                     background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\
-                     stop: 0 #4d8e0d, stop: 1 #5aa50f);\
-                     }\
-                     QPushButton:hover {\
-                     background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\
-                     stop: 0 #a5a5a5, stop: 0.7 #4d4d4d, stop: 1 #747474);\
-                     color: white;\
-                     }\
-                     QPushButton:default {\
-                     border: 1px solid #0e5a00;\
-                     background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\
-                     stop: 0 #75ce1f, stop: 0.8 #4d8e0d, stop: 1 #5aa50f);\
-                     }\
-                     QPushButton:default:hover {\
-                     background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\
-                     stop: 0 #75ce1f, stop: 0.7 #4d8e0d, stop: 1 #7ac52f);\
-                     }\
-                     QDialog{\
-                     padding: 3.5em 2em 1em 1em;\
-                     background: url(:/globalStyle/work_area.png) no-repeat top left;\
-                     border: none;\
-                     font-size: 10pt;\
-                     min-width: 150px;\
-                     }\
-                     QMenuBar{\
-                     background: url(:/globalStyle/work_area.png) no-repeat top left;\
-                     border: none;\
-                     font-size: 10pt;\
-                     }\
-                     QToolBar{\
-                     background: url(:/globalStyle/background.png) no-repeat top left;\
-                     border: none;\
-                     font-size: 10pt;\
-                     }\
-                     QTextBrowser, QTabWidget{\
-                     background: url(:/globalStyle/background.png) no-repeat top left;\
-                     border: none;\
-                     font-size: 10pt;\
-                     color: white;\
-                     }";
-//                      QMenu::item:selected { \
-//                      background-color: white;\
-//                      color: black;\
-//                      }\
-//                      QLabel{\
-//                      color: rgb(255, 255, 255);\
-//                      }";
+QString globalStyle = "\
+    QPushButton {\
+        border: 1px solid #313131;\
+        border-radius: 4px;\
+        background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\
+        stop: 0 #a5a5a5, stop: 0.8 #4d4d4d, stop: 1 #545454);\
+        padding: 4px 12px;\
+        color: #eeeeee;\
+        font-weight: bold;\
+    }\
+    QPushButton:disabled, QPushButton:default:disabled {\
+        color: rgba(255,255,255,80);\
+        border: 1px solid rgba(0,0,0,80);\
+        background: none;\
+    }\
+    QPushButton:default:pressed, QPushButton:pressed {\
+        border: 1px solid #0e5a00;\
+        background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\
+        stop: 0 #4d8e0d, stop: 1 #5aa50f);\
+    }\
+    QPushButton:hover {\
+        background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\
+        stop: 0 #a5a5a5, stop: 0.7 #4d4d4d, stop: 1 #747474);\
+        color: white;\
+    }\
+    QPushButton:default {\
+        border: 1px solid #0e5a00;\
+        background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\
+        stop: 0 #75ce1f, stop: 0.8 #4d8e0d, stop: 1 #5aa50f);\
+    }\
+    QPushButton:default:hover {\
+        background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\
+        stop: 0 #75ce1f, stop: 0.7 #4d8e0d, stop: 1 #7ac52f);\
+    }\
+    QDialog{\
+        padding: 3.5em 2em 1em 1em;\
+        background: url(:/globalStyle/work_area.png) no-repeat top left;\
+        border: none;\
+        font-size: 10pt;\
+        min-width: 150px;\
+    }\
+    QMenuBar{\
+        border: none;\
+        font-size: 10pt;\
+    }\
+    QMenu{\
+        color: rgb(255, 255, 255);\
+        alternate-background-color: rgb(127, 127, 127);\
+        background: url(:/globalStyle/BG_DARK) top left;\
+    }\
+    QToolBar{\
+        background: url(:/globalStyle/BG_DARK) top left;\
+        border: none;\
+        font-size: 10pt;\
+    }\
+    QWidget#centralwidget{\
+        background-image: url(:/globalStyle/BG_DARK);\
+    }\
+    QTextBrowser{\
+        font-size: 10pt;\
+        background-image: url(:/globalStyle/BG_LIGHT);\
+    }";
 
-//   /* odd dropdown button widget */
-//      QPushButton::menu-indicator {
-//        subcontrol-origin: margin;
-//        left: -7px;
-//        subcontrol-position: right;
-//   }
-
-//        QPushButton:open { /* when the button has its menu open */
-//        background: #444444;
-//        border: 1px solid #222222;
-//        color: white;
-//      }
-//
-//         QPushButton::menu-indicator {
-//        image: url(arr_down.png);
 /*
-  subcontrol-origin: padding;
-  subcontrol-position: bottom right;
-*/
-//      }
+    QMenu::item:selected { \
+        background-color: white;\
+        color: black;\
+    }\
+    QLabel{\
+        color: rgb(255, 255, 255);\
+    }\
 
-//         QPushButton::menu-indicator:pressed, QPushButton::menu-indicator:open {
-//          position: relative;
-//          top: 1px; left: 1px; /* shift the arrow by 1 px */
-//      }
-// ";*/
+    // odd dropdown button widget
+    QPushButton::menu-indicator {\
+        subcontrol-origin: margin;\
+        left: -7px;\
+        subcontrol-position: right;\
+    }\
+
+    QPushButton:open {\ // when the button has its menu open
+        background: #444444;\
+        border: 1px solid #222222;\
+        color: white;\
+    }\
+
+    QPushButton::menu-indicator {\
+        image: url(arr_down.png);\
+        subcontrol-origin: padding;\
+        subcontrol-position: bottom right;\
+    }\
+
+    QPushButton::menu-indicator:pressed, QPushButton::menu-indicator:open {\
+        position: relative;\
+        top: 1px; left: 1px;\ // shift the arrow by 1 px
+    }\
+*/
